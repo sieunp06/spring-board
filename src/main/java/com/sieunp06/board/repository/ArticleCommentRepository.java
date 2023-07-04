a76813be9b2b7bf1e0acf9756660badf0f90dfcc
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-07-05 03:24:48 +0900

    feat: data-rest 설정

--- src/main/java/com/sieunp06/board/repository/ArticleCommentRepository.java
+++ src/main/java/com/sieunp06/board/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.sieunp06.board.repository;
 
 import com.sieunp06.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
