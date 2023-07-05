9e9c65f36a1cf1173882fac72f1bf90ad3109157
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-07-05 13:37:43 +0900

    feat: Querydsl을 이용하여 api 검색 옵션 추가

--- src/main/java/com/sieunp06/board/repository/ArticleRepository.java
+++ src/main/java/com/sieunp06/board/repository/ArticleRepository.java
@@ -1,9 +1,29 @@
 package com.sieunp06.board.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.sieunp06.board.domain.Article;
+import com.sieunp06.board.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>, // 기본 검색 기능
+        QuerydslBinderCustomizer<QArticle>  // 입맛에 맞도록 검색 기능을 수정함.
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);   // article에 모든 필드에 대해 검색 대상을 제한 시킴.
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // 검색 parameter는 하나만 받음, 대소문자 구분 x
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
