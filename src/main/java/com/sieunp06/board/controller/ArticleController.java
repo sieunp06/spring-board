7c3e51b2e797f043a5173f719d292900c5f40195
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-07-09 12:38:57 +0900

    feat: 검색 기능 구현 밑작업

--- src/main/java/com/sieunp06/board/controller/ArticleController.java
+++ src/main/java/com/sieunp06/board/controller/ArticleController.java
@@ -47,7 +47,7 @@ public class ArticleController {
 
         map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumbers);
-        map.addAttribute("totalCount", articleService.getArticleCount());
+        map.addAttribute("searchTypes", SearchType.values());
 
         return "articles/index";
     }
@@ -57,6 +57,8 @@ public class ArticleController {
         ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
         map.addAttribute("article", article);
         map.addAttribute("articleComments", article.articleCommentsResponses());
+        map.addAttribute("totalCount", articleService.getArticleCount());
+
         return "articles/detail";
     }
 }
--- src/test/java/com/sieunp06/board/controller/ArticleControllerTest.java
+++ src/test/java/com/sieunp06/board/controller/ArticleControllerTest.java
@@ -1,6 +1,7 @@
 package com.sieunp06.board.controller;
 
 import com.sieunp06.board.config.SecurityConfig;
+import com.sieunp06.board.domain.type.SearchType;
 import com.sieunp06.board.dto.ArticleWithCommentsDto;
 import com.sieunp06.board.dto.UserAccountDto;
 import com.sieunp06.board.service.ArticleService;
@@ -57,6 +58,30 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"))
                 .andExpect(model().attributeExists("paginationBarNumbers"));
         then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 검색어와 함께 호출")
+    @Test
+    public void givenSearchKeyword_whenSearchingArticlesView_thenReturnsArticlesView() throws Exception {
+        // Given
+        SearchType searchType = SearchType.TITLE;
+        String searchValue = "title";
+        given(articleService.searchArticles(eq(searchType), eq(searchValue), any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
+
+        // When & Then
+        mvc.perform(
+                        get("/articles")
+                                .queryParam("searchType", searchType.name())
+                                .queryParam("searchValue", searchValue)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("searchTypes"));
+        then(articleService).should().searchArticles(eq(searchType), eq(searchValue), any(Pageable.class));
         then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
     }
 
